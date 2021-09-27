691e65eac10e19a120586dfc46fba4c108994368
PullStackPigDev <90464060+PullStackPigDev@users.noreply.github.com>
2021-09-27 11:34:33 -0400

    Create twoSum.cpp

---
+++ twoSum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i+1; j < nums.size(); ++j) {
+                if (nums[i]+nums[j] == target) {
+                    vector<int> name {i, j};
+                    return name;
+                }
+            }
+        }
+        return nums;
+    }
+};
